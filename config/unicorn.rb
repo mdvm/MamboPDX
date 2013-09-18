b310a19157c61830783f6ad4fc3941085ccea726
DeuceBag33 <deucebag33@deucebag33-laptop.(none)>
2013-09-18 00:28:16 -0700

    Deployment config filesAgain

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/mambo/current"
+root = "/home/deployer/apps/MamboPDX/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.MamboPDX.sock"
 worker_processes 2
 timeout 30
