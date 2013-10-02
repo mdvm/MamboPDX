7cdcbbd4185f6ab119933f4b5edc76648972044f
DeuceBag33 <deucebag33@deucebag33-laptop.(none)>
2013-10-02 09:57:44 -0700

    coming Soon index and edge files

---
+++ public/mamboComingSoon_edgeActions.js
@@ -0,0 +1,8 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",8317,function(sym,e){sym.stop();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-138944435");
